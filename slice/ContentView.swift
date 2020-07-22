f1b92d534b35f36198ddd382c7cea6774518e920
MoNireu <monireu.dev@gmail.com>
2020-07-22 19:37:23 +0900

    Initial Commit

---
+++ slice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slice
+//
+//  Created by MoNireu on 2020/07/22.
+//  Copyright © 2020 monireu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
